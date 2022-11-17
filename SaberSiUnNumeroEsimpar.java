63295f7f1268390f47ccdfdde82638db57895c0c
NOt NAme <118314426+SSandwormm@users.noreply.github.com>
2022-11-17 13:22:03 -0500

    Add files via upload

---
+++ SaberSiUnNumeroEsimpar.java
@@ -0,0 +1,33 @@
+package sabersiunnumeroesimpar;
+
+import java.util.Scanner;
+
+public class SaberSiUnNumeroEsimpar {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Ingrese el valor: ");
+        int numero;
+        numero = keyboard.nextInt();
+
+        System.out.println(si_y_no(numero));
+
+    }
+    static boolean si_y_no(int numero) {
+        if (numero == 0) {
+            System.out.println(" el numero es par");
+            return true;
+        }
+        else if (numero == 1 || numero == -1) {
+            System.out.println("el numero no es par");        
+            return false;
+        }
+        else if (numero > 1) {
+            return si_y_no(numero - 2);
+        }
+        else {
+            return si_y_no(numero + 2);
+        }
+    }
+}
