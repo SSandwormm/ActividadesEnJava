3fd9f2a7e3a19dd3e6f44b13fa65d4542b197695
NOt NAme <118314426+SSandwormm@users.noreply.github.com>
2022-11-17 13:13:41 -0500

    Add files via upload

---
+++ SaberSiUnNumeroEsimpar.java
@@ -0,0 +1,22 @@
+package sabersiunnumeroesimpar;
+
+import java.util.Scanner;
+
+public class SaberSiUnNumeroEsimpar {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int numero;
+
+        System.out.print("Por favor ingrese el número a evaluar: ");
+        numero = teclado.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es un número par.");
+        } else {
+            System.out.println(numero  + " no es un número par.");
+        }
+    }
+
+}
